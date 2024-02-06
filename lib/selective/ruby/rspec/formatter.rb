e66ba4d39471daf4ecdc00031701a50fa493c441
Benjamin Wood <benjaminwood@gmail.com>
2024-02-06 17:28:34 +0000

    Fix bug causing test failures when reconnect occurs
    This regression happened when we implemented the formatter. Because
    RSpec is calling the formatter methods, we need to ensure we handle
    connection errors.
    
    Co-authored-by: Nate Vick <nate.vick@hint.io>

--- lib/selective/ruby/rspec/formatter.rb
+++ lib/selective/ruby/rspec/formatter.rb
@@ -6,17 +6,20 @@ module Selective
 
         def initialize(...); end
 
-        def self.callback=(callback)
-          @callback = callback
+        def self.runner_wrapper=(runner_wrapper)
+          @runner_wrapper = runner_wrapper
         end
 
-        def self.callback
-          @callback
+        def self.runner_wrapper
+          @runner_wrapper
         end
         
         %i(example_passed example_failed example_pending).each do |method|
           define_method(method) do |notification|
-            self.class.callback.call(notification.example)
+            self.class.runner_wrapper.report_example(notification.example)
+          rescue Selective::Ruby::Core::ConnectionLostError
+            ::RSpec.world.wants_to_quit = true
+            self.class.runner_wrapper.remove_test_case_result(notification.example.id)
           end
         end
       end
